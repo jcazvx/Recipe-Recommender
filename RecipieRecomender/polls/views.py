106feba28929b8ca7555eb9bbd88a2380c8305fb
Chunleshy <61976038+Chunleshy@users.noreply.github.com>
2023-06-22 10:01:09 -0500

    Update views.py

--- RecipieRecomender/polls/views.py
+++ RecipieRecomender/polls/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index. This is Ellie.")
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index. This is Jade.")
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index. This is Kunle.")
